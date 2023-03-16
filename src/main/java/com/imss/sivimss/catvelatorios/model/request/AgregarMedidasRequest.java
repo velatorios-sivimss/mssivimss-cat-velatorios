089b3f50bbdbe299c4a491adad65863f17d75caf
Eduardo Orlando <eduardo.flores@people-media.com.mx>
2023-03-16 14:01:55 -0600

    carga inicial proyecto

---
+++ src/main/java/com/imss/sivimss/catvelatorios/model/request/AgregarMedidasRequest.java
@@ -0,0 +1,12 @@
+package com.imss.sivimss.catvelatorios.model.request;
+
+import lombok.Data;
+
+@Data
+public class AgregarMedidasRequest {
+private String idArticulo;
+private String largo;
+private String ancho;
+private String alto;
+
+}
