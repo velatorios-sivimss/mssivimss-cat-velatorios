7b52760b4a6b68196a9a4bd97a3a4cf2ef8d884f
EduardoGlez16 <105452946+EduardoGlez16@users.noreply.github.com>
2023-03-23 16:13:35 -0600

    Se agrego resilencia

--- src/main/java/com/imss/sivimss/catvelatorios/controller/GestionarVelatoriosController.java
+++ src/main/java/com/imss/sivimss/catvelatorios/controller/GestionarVelatoriosController.java
@@ -1,7 +1,11 @@
 package com.imss.sivimss.catvelatorios.controller;
 
 import java.io.IOException;
+import java.util.concurrent.CompletableFuture;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -10,8 +14,13 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.imss.sivimss.catvelatorios.service.GestionarVelatorioService;
 import com.imss.sivimss.catvelatorios.util.DatosRequest;
+import com.imss.sivimss.catvelatorios.util.ProviderServiceRestTemplate;
 import com.imss.sivimss.catvelatorios.util.Response;
 
+import io.github.resilience4j.circuitbreaker.CallNotPermittedException;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
+import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
 import lombok.AllArgsConstructor;
 
 @AllArgsConstructor
@@ -21,6 +30,12 @@ public class GestionarVelatoriosController {
 	
 	private GestionarVelatorioService velatorioService;
 	
+	@Autowired
+	private ProviderServiceRestTemplate providerRestTemplate;
+
+	@CircuitBreaker(name = "msflujo", fallbackMethod = "fallbackGenerico")
+	@Retry(name = "msflujo", fallbackMethod = "fallbackGenerico")
+	@TimeLimiter(name = "msflujo")
 	@PostMapping("/catalogo")
 	public Response<?> catalogo(@RequestBody DatosRequest request,Authentication authentication) throws IOException {
 	
@@ -28,39 +43,93 @@ public class GestionarVelatoriosController {
       
 	}
 	
+	@CircuitBreaker(name = "msflujo", fallbackMethod = "fallbackGenerico")
+	@Retry(name = "msflujo", fallbackMethod = "fallbackGenerico")
+	@TimeLimiter(name = "msflujo")
 	@PostMapping("/buscar")
-	public Response<?> buscar(@RequestBody DatosRequest request,Authentication authentication) throws IOException {
+	public CompletableFuture<?> buscar(@RequestBody DatosRequest request,Authentication authentication) throws IOException {
 	
-		return velatorioService.buscarVelatorio(request,authentication);
+		Response<?> response = velatorioService.buscarVelatorio(request,authentication);
+		return CompletableFuture
+				.supplyAsync(() -> new ResponseEntity<>(response, HttpStatus.valueOf(response.getCodigo())));
+
       
 	}
 	
+	@CircuitBreaker(name = "msflujo", fallbackMethod = "fallbackGenerico")
+	@Retry(name = "msflujo", fallbackMethod = "fallbackGenerico")
+	@TimeLimiter(name = "msflujo")
 	@PostMapping("/filtros")
-	public Response<?> buscarDelegacion(@RequestBody DatosRequest request,Authentication authentication) throws IOException {
+	public CompletableFuture<?> buscarDelegacion(@RequestBody DatosRequest request,Authentication authentication) throws IOException {
 	
-		return velatorioService.buscarVelatorioDelegacion(request,authentication);
+		Response<?> response = velatorioService.buscarVelatorioDelegacion(request,authentication);
+		return CompletableFuture
+				.supplyAsync(() -> new ResponseEntity<>(response, HttpStatus.valueOf(response.getCodigo())));
+
       
 	}
-	
+	@CircuitBreaker(name = "msflujo", fallbackMethod = "fallbackGenerico")
+	@Retry(name = "msflujo", fallbackMethod = "fallbackGenerico")
+	@TimeLimiter(name = "msflujo")
 	@PostMapping("/agregar")
-	public Response<?> agregar(@RequestBody DatosRequest request,Authentication authentication) throws IOException {
+	public CompletableFuture<?> agregar(@RequestBody DatosRequest request,Authentication authentication) throws IOException {
 	
-		return velatorioService.agregarVelatorio(request,authentication);
+		Response<?> response = velatorioService.agregarVelatorio(request,authentication);
+		return CompletableFuture
+				.supplyAsync(() -> new ResponseEntity<>(response, HttpStatus.valueOf(response.getCodigo())));
+
       
 	}
-	
+	@CircuitBreaker(name = "msflujo", fallbackMethod = "fallbackGenerico")
+	@Retry(name = "msflujo", fallbackMethod = "fallbackGenerico")
+	@TimeLimiter(name = "msflujo")
 	@PostMapping("/actualiza")
-	public Response<?> actualizar(@RequestBody DatosRequest request,Authentication authentication) throws IOException {
+	public CompletableFuture<?> actualizar(@RequestBody DatosRequest request,Authentication authentication) throws IOException {
 	
-		return velatorioService.actualizarVelatorio(request,authentication);
+		Response<?> response = velatorioService.actualizarVelatorio(request,authentication);
+		return CompletableFuture
+				.supplyAsync(() -> new ResponseEntity<>(response, HttpStatus.valueOf(response.getCodigo())));
+
       
 	}
-	
+	@CircuitBreaker(name = "msflujo", fallbackMethod = "fallbackGenerico")
+	@Retry(name = "msflujo", fallbackMethod = "fallbackGenerico")
+	@TimeLimiter(name = "msflujo")
 	@PostMapping("/estatus")
-	public Response<?> cambiarEstatus(@RequestBody DatosRequest request,Authentication authentication) throws IOException {
+	public CompletableFuture<?> cambiarEstatus(@RequestBody DatosRequest request,Authentication authentication) throws IOException {
 	
-		return velatorioService.cambiarEstatusVelatorio(request,authentication);
+		Response<?> response = velatorioService.cambiarEstatusVelatorio(request,authentication);
+		return CompletableFuture
+				.supplyAsync(() -> new ResponseEntity<>(response, HttpStatus.valueOf(response.getCodigo())));
+
       
 	}
+	
+	/**
+	 * fallbacks generico
+	 * 
+	 * @return respuestas
+	 */
+	private CompletableFuture<?> fallbackGenerico(@RequestBody DatosRequest request, Authentication authentication,
+			CallNotPermittedException e) {
+		Response<?> response = providerRestTemplate.respuestaProvider(e.getMessage());
+		return CompletableFuture
+				.supplyAsync(() -> new ResponseEntity<>(response, HttpStatus.valueOf(response.getCodigo())));
+	}
+
+	private CompletableFuture<?> fallbackGenerico(@RequestBody DatosRequest request, Authentication authentication,
+			RuntimeException e) {
+		Response<?> response = providerRestTemplate.respuestaProvider(e.getMessage());
+		return CompletableFuture
+				.supplyAsync(() -> new ResponseEntity<>(response, HttpStatus.valueOf(response.getCodigo())));
+	}
+
+	private CompletableFuture<?> fallbackGenerico(@RequestBody DatosRequest request, Authentication authentication,
+			NumberFormatException e) {
+		Response<?> response = providerRestTemplate.respuestaProvider(e.getMessage());
+		return CompletableFuture
+				.supplyAsync(() -> new ResponseEntity<>(response, HttpStatus.valueOf(response.getCodigo())));
+	}
+
 
 }
