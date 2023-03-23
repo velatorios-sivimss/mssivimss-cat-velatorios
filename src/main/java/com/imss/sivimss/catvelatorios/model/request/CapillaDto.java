ab921d348a060decab4f766479bacf785008b388
esanvicente <erick.sanvicente@people-media.com.mx>
2023-03-23 17:11:31 -0600

    Se elimina el uso de @NotNull, porque no se estaba encontrando en las versiones de otros colaboradores, con dicha anotacion.

--- src/main/java/com/imss/sivimss/catvelatorios/model/request/CapillaDto.java
+++ src/main/java/com/imss/sivimss/catvelatorios/model/request/CapillaDto.java
@@ -6,26 +6,20 @@ import lombok.Setter;
 
 import javax.validation.constraints.NotNull;
 
+
 @Getter
 @Setter
 @EqualsAndHashCode(callSuper = false)
 public class CapillaDto {
+    @NotNull
     private Long idCapilla;
-    @NotNull
     private String nombre;
-    @NotNull
     private Integer capacidad;
-    @NotNull
     private Double largo;
-    @NotNull
     private Double alto;
-    @NotNull
     private Double areaTotal;
-    // todo - hay que mapear los datos del velatorio, buscar a la persona que esta haciendo esa parte
-    @NotNull
     private Long idVelatorio;
     private String nombreVelatorio;
-    @NotNull
     private Boolean estatus;
 
 }
