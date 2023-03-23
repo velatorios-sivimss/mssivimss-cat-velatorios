b33e17bfaac2607061366001e7864dc30122942e
esanvicente <erick.sanvicente@people-media.com.mx>
2023-03-23 17:18:47 -0600

    Se hacen las correcciones para las funciones NOW() y la logica del filtrado de los parametros opcionales.

--- src/main/java/com/imss/sivimss/catvelatorios/beans/Capilla.java
+++ src/main/java/com/imss/sivimss/catvelatorios/beans/Capilla.java
@@ -13,7 +13,8 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Capilla {
-    private CapillaDto capilla;
+
+    private static final String WHERE = "WHERE ";
 
     /**
      * Inserta una capilla con los campos necesarios para su creacion
@@ -33,7 +34,7 @@ public class Capilla {
         queryHelper.agregarParametroValues("ID_VELATORIO", String.valueOf(capillaDto.getIdVelatorio()));
         queryHelper.agregarParametroValues("CVE_ESTATUS", String.valueOf(1));
         queryHelper.agregarParametroValues("ID_USUARIO_ALTA", String.valueOf(usuario.getId()));
-        queryHelper.agregarParametroValues("FEC_ALTA", "NOW()");
+        queryHelper.agregarParametroValues("FEC_ALTA", "CURRENT_TIMESTAMP");
         queryHelper.agregarParametroValues("ID_USUARIO_MODIFICA", "null");
         queryHelper.agregarParametroValues("FEC_ACTUALIZACION", "null");
         queryHelper.agregarParametroValues("ID_USUARIO_BAJA", "null");
@@ -56,6 +57,7 @@ public class Capilla {
      */
     public DatosRequest actualizar(CapillaDto capillaDto, UsuarioDto usuario) {
         DatosRequest datos = new DatosRequest();
+
         QueryHelper queryHelper = new QueryHelper("UPDATE SVC_CAPILLA");
 
         queryHelper.agregarParametroValues("NOM_CAPILLA", "'" + capillaDto.getNombre() + "'");
@@ -64,7 +66,7 @@ public class Capilla {
         queryHelper.agregarParametroValues("NUM_ALTO", String.valueOf(capillaDto.getAlto()));
         queryHelper.agregarParametroValues("ID_VELATORIO", String.valueOf(capillaDto.getIdVelatorio()));
         queryHelper.agregarParametroValues("ID_USUARIO_MODIFICA", String.valueOf(usuario.getId()));
-        queryHelper.agregarParametroValues("FEC_ACTUALIZACION", "NOW()");
+        queryHelper.agregarParametroValues("FEC_ACTUALIZACION", "CURRENT_TIMESTAMP");
         // todo - se puede detallar mas el where para que reciba mas parametros que nos diga el tipo de comparacion etc.
         queryHelper.addWhere("ID_CAPILLA = " + capillaDto.getIdCapilla());
 
@@ -82,7 +84,7 @@ public class Capilla {
         DatosRequest datos = new DatosRequest();
         String query = "UPDATE SVC_CAPILLA SET " +
                 "CVE_ESTATUS = !CVE_ESTATUS, " +
-                "FEC_BAJA = now(), " +
+                "FEC_BAJA = CURRENT_TIMESTAMP, " +
                 "ID_USUARIO_BAJA = " + usuario.getId() +
                 " WHERE ID_CAPILLA = " + idCapilla;
 
@@ -104,23 +106,63 @@ public class Capilla {
     public DatosRequest consultarCapillas(DatosRequest request) {
         Gson gson = new Gson();
         FiltrosCapillas filtros = gson.fromJson(String.valueOf(request.getDatos().get(AppConstantes.DATOS)), FiltrosCapillas.class);
-        String query = "SELECT " +
-                "ID_CAPILLA AS capillaId, " +
-                "NOM_CAPILLA AS nombre, " +
-                "CAN_CAPACIDAD AS capacidad, " +
-                "NUM_LARGO AS largo, " +
-                "NUM_ALTO AS alto, " +
-                "CVE_ESTATUS AS estatus, " +
-                "velatorio.ID_VELATORIO AS velatorioId, " +
-                "velatorio.NOM_VELATORIO AS nombreVelatorio " +
-                "FROM SVC_CAPILLA capilla " +
-                "JOIN SVC_VELATORIO velatorio ON capilla.ID_VELATORIO = velatorio.ID_VELATORIO " +
-                "WHERE " +
-                "(" + filtros.getIdVelatorio() + " IS NULL OR capilla.ID_VELATORIO = " + filtros.getIdVelatorio() + ") " +
-                "AND (" + filtros.getIdCapilla() + " IS NULL OR capilla.ID_CAPILLA = " + filtros.getIdCapilla() + ") " +
-                "AND (" + filtros.getNombreCapilla() + " IS NULL OR capilla.NOM_CAPILLA = " + filtros.getNombreCapilla() + ") " +
-                "GROUP BY ID_CAPILLA";
+
+        StringBuilder queryBuilder = new StringBuilder("SELECT ");
+        queryBuilder.append("ID_CAPILLA AS capillaId, ");
+        queryBuilder.append("NOM_CAPILLA AS nombre, ");
+        queryBuilder.append("CAN_CAPACIDAD AS capacidad, ");
+        queryBuilder.append("NUM_LARGO AS largo, ");
+        queryBuilder.append("NUM_ALTO AS alto, ");
+        queryBuilder.append("CVE_ESTATUS AS estatus, ");
+        queryBuilder.append("velatorio.ID_VELATORIO AS velatorioId, ");
+        queryBuilder.append("velatorio.NOM_VELATORIO AS nombreVelatorio ");
+        queryBuilder.append("FROM SVC_CAPILLA capilla ");
+        queryBuilder.append("JOIN SVC_VELATORIO velatorio ON capilla.ID_VELATORIO = velatorio.ID_VELATORIO ");
+
+        if (filtros.getIdCapilla() != null) {
+            queryBuilder.append(WHERE);
+            queryBuilder.append("capilla.id_capilla = ").append(filtros.getIdCapilla());
+        }
+        if (filtros.getNombreCapilla() != null) {
+
+            if (queryBuilder.toString().contains(WHERE)) {
+                queryBuilder.append(" capilla.NOM_CAPILLA = ").append(filtros.getNombreCapilla());
+            } else {
+                queryBuilder.append(WHERE)
+                        .append(" capilla.NOM_CAPILLA = ").append(filtros.getNombreCapilla());
+            }
+
+        }
+        if (filtros.getIdVelatorio() != null) {
+            if (queryBuilder.toString().contains(WHERE)) {
+                queryBuilder.append("capilla.ID_VELATORIO = ").append(filtros.getIdVelatorio());
+            } else {
+                queryBuilder.append(WHERE)
+                        .append("capilla.ID_VELATORIO = ").append(filtros.getIdVelatorio());
+            }
+        }
+
+//        queryHelper.obtenerQueryActualizar()
+//        String query = "SELECT " +
+//                "ID_CAPILLA AS capillaId, " +
+//                "NOM_CAPILLA AS nombre, " +
+//                "CAN_CAPACIDAD AS capacidad, " +
+//                "NUM_LARGO AS largo, " +
+//                "NUM_ALTO AS alto, " +
+//                "CVE_ESTATUS AS estatus, " +
+//                "velatorio.ID_VELATORIO AS velatorioId, " +
+//                "velatorio.NOM_VELATORIO AS nombreVelatorio " +
+//                "FROM SVC_CAPILLA capilla " +
+//                "JOIN SVC_VELATORIO velatorio ON capilla.ID_VELATORIO = velatorio.ID_VELATORIO " +
+//                "WHERE " +
+//                "(" + filtros.getIdVelatorio() + " IS NULL OR capilla.ID_VELATORIO = " + filtros.getIdVelatorio() + ") " +
+//                "AND (" + filtros.getIdCapilla() + " IS NULL OR capilla.ID_CAPILLA = " + filtros.getIdCapilla() + ") " +
+//                "AND (" + filtros.getNombreCapilla() + " IS NULL OR capilla.NOM_CAPILLA = " + filtros.getNombreCapilla() + ") " +
+//                "GROUP BY ID_CAPILLA";
         Map<String, Object> parametros = new HashMap<>();
+        String query = queryBuilder.toString();
+        System.out.println("********************************");
+        System.out.println(query);
         parametros.put(
                 AppConstantes.QUERY,
                 DatatypeConverter.printBase64Binary(query.getBytes())
@@ -149,7 +191,7 @@ public class Capilla {
                 "velatorio.NOM_VELATORIO AS nombreVelatorio " +
                 "FROM SVC_CAPILLA capilla " +
                 "JOIN SVC_VELATORIO velatorio ON capilla.ID_VELATORIO = velatorio.ID_VELATORIO " +
-                "WHERE " +
+                WHERE +
                 "ID_CAPILLA = " + idCapilla +
                 " GROUP BY ID_CAPILLA";
         Map<String, Object> parametros = new HashMap<>();
